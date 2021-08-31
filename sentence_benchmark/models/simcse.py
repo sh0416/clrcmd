038403de66a8ed9ac674a0c4c69673e6574d3b46
Seonghyeon <seonghyeon.drew@gmail.com>
2021-08-31 12:43:21 +0900

    평균 풀링으로 변경

--- sentence_benchmark/models/simcse.py
+++ sentence_benchmark/models/simcse.py
@@ -3,6 +3,7 @@ from typing import Dict, List
 import numpy as np
 import torch
 import torch.nn.functional as F
+from torch import Tensor
 from transformers import AutoModel, AutoTokenizer
 
 from sentence_benchmark.data import Input
@@ -38,7 +39,46 @@ def batcher(inputs: List[Input], param: Dict) -> np.ndarray:
         outputs2 = param["model"](
             **batch2, output_hidden_states=True, return_dict=True
         )
-        outputs1 = outputs1.last_hidden_state[:, 0, :]
-        outputs2 = outputs2.last_hidden_state[:, 0, :]
+        # outputs1 = outputs1.last_hidden_state[:, 0, :]
+        # outputs2 = outputs2.last_hidden_state[:, 0, :]
+
+        def masked_sum(x: Tensor, mask: Tensor, dim: int) -> Tensor:
+            """Sum (masked version)
+
+            :param x: Input tensor
+            :type x: Tensor
+            :param mask: Mask tensor. Could be broadcastable.
+            :type mask: Tensor
+            :param dim:
+            :type dim: int
+            :return: Result of sum
+            :rtype: Tensor
+            """
+            return torch.sum(x * mask.float(), dim=dim)
+
+        def masked_mean(x: Tensor, mask: Tensor, dim: int) -> Tensor:
+            """Mean (masked version)
+
+            :param x: Input tensor
+            :type x: Tensor
+            :param mask: Mask tensor. Could be broadcastable.
+            :type mask: Tensor
+            :param dim:
+            :type dim: int
+            :return: Result of mean
+            :rtype: Tensor
+            """
+            return masked_sum(x, mask, dim) / torch.sum(mask.float(), dim)
+
+        outputs1 = masked_mean(
+            outputs1.last_hidden_state,
+            batch1["attention_mask"][:, :, None],
+            dim=1,
+        )
+        outputs2 = masked_mean(
+            outputs2.last_hidden_state,
+            batch2["attention_mask"][:, :, None],
+            dim=1,
+        )
         score = F.cosine_similarity(outputs1, outputs2, dim=1)
         return score.cpu().numpy()
